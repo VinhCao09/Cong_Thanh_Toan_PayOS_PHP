d0fa9340113c86e2426d8e30fb5ea9488decb3c2
VinhCao09 <92619586+VinhCao09@users.noreply.github.com>
2024-11-05 13:32:35 +0700

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -6,12 +6,12 @@ require_once __DIR__ . '/vendor/autoload.php';
 use PayOS\PayOS;
 
 // Keep your PayOS key protected by including it by an env variable
-$payOSClientId = 'f5e38c0b-03db-4892-a0c8-7b2cc91df07a';
-$payOSApiKey = 'c909e88d-a836-4fd5-a7ff-692f8ebf481f';
-$payOSChecksumKey = '4cf3248785bd3fde18bebfd1d6f1bc21f3a306a6ff96daa9a475f5eb7b2ba52a';
+$payOSClientId = 'MA CUA BAN';
+$payOSApiKey = 'MA CUA BAN';
+$payOSChecksumKey = 'MA CUA BAN';
 
 $payOS = new PayOS($payOSClientId, $payOSApiKey, $payOSChecksumKey);
-
+// Hãy thay thế domain chính xác vào đây nhé
 $YOUR_DOMAIN = 'http://vinhcao.9.9.9.9.server-test.local:3000';
 $DOMAN_SUCESS = 'http://vinhcao.9.9.9.9.server-test.local:3000/success.html';
 
